75f0befb52ec5bb470943a23be59b1310811da61
shepherd <1286479393@qq.com>
2020-07-21 22:40:53 +0800

    在线教育-谷粒学院-前端环境搭建-第一次提交

---
+++ guli-admin/src/views/teacher/form.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="app-container">
+    新增讲师
+  </div>
+</template>
---
+++ guli-admin/src/views/teacher/list.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app-container">
+    <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="讲师姓名"
+        width="180"/>
+      <el-table-column
+        prop="level"
+        label="头衔"
+        width="180"/>
+      <el-table-column
+        prop="joinDate"
+        label="入驻时间"/>
+    </el-table>
+  </div>
+</template>
